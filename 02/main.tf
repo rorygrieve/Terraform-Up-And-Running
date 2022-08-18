510db35dfa40dc26b35996dd9882849b2c67d1f9
Rory Grieve <rory.grieve@armakuni.com>
2022-08-18 17:30:45 +0100

    EC2 instance with hello world

--- 02/main.tf
+++ 02/main.tf
@@ -1,20 +1,43 @@
 resource "aws_instance" "example" {
-  ami           = "ami-09a2a0f7d2db8baca"
-  instance_type = "t2.micro"
+  ami                    = "ami-09a2a0f7d2db8baca"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
 }
 
 provider "aws" {
-      # profile = "default"
-      region  = "eu-west-2"
+  region = "eu-west-2"
 }
 
-# terraform {
-#       required_providers {
-#          aws = {
-#              source  = "hashicorp/aws"
-#              #version = "~> 3.21" # Optional but recommended in production
-#         }
-#      }
-#  }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
 
-# ap southeast-2
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
